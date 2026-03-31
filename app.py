ccc23d87c3d4f927b20641db378ec4ce45b6c77a
dceleste <danceleste974@gmail.com>
2026-03-31 13:06:00 +0000

    contact

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ def hello_world():
     return render_template('hello.html')
 
 # Déposez votre code à partir d'ici :
-
+@app.route("/contact")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"
 
 
 
 
 
 # Ne rien mettre après ce commentaire
-    
+
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5000, debug=True)
